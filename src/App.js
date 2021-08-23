6f7daeb90ba27926a8484fa93372ac9d15289912
mata <mata@matas-MacBook-Air.local>
2021-08-24 00:25:32 +0200

    testing open weather api no styling

--- src/App.js
+++ src/App.js
@@ -1,23 +1,55 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import axios from "axios";
+import { useEffect, useRef, useState } from "react";
 
 function App() {
+  const [weatherInfo, setWeatherInfo] = useState(null);
+  const inputRef = useRef(null);
+
+  useEffect(() => {
+    fetchWeatherInfo();
+  }, []);
+
+  const fetchWeatherInfo = (e) => {
+    e?.preventDefault();
+  };
+
+  const options = {
+    method: "GET",
+    url: "https://community-open-weather-map.p.rapidapi.com/climate/month",
+    params: {
+      q: inputRef.current?.value || "London",
+      units: "metric",
+    },
+
+    headers: {
+      "x-rapidapi-host": "community-open-weather-map.p.rapidapi.com",
+      "x-rapidapi-key": "b1b3051765msh91cdea31d8cdde3p1fd01fjsn9e846f5e9525",
+    },
+  };
+
+  const response = axios
+    .request(options)
+    .then((response) => {
+      console.log(response.data);
+      setWeatherInfo(response.data)
+    })
+    .catch((error) => {
+      alert(error.message);
+    });
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Our wetaher app</h1>
+      <form>
+        <input ref={inputRef} type="text" placeholder="Type the city" />
+        <button onClick={fetchWeatherInfo} type="submit">
+          Show me the weather
+        </button>
+      </form>
+
+      <h2>{weatherInfo?.name}</h2>
+      <h3>{weatherInfo?.sys?.sunrise}</h3>
     </div>
   );
 }
